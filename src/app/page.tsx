0e0a2000809381b0d49fa488204b72b9128a6076
Mahdi Touiserkani <mahditouiserkani78@gmail.com>
2024-02-11 15:57:40 +0330

    Implement : little changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <Stack horizontal tokens={{ childrenGap: 5 }} style={{ overflowX: 'auto', maxWidth: '100%' }}>
       <Stack grow style={{ position: 'relative' }}>
-        <Text style={{ fontSize: '2rem', marginLeft: '25px', userSelect: 'none' }}>Home</Text>
+        <Text style={{ fontSize: '3rem', marginLeft: '25px', userSelect: 'none' }}>Home</Text>
         {
           <Stack verticalAlign="center" horizontalAlign="center" style={{ height: '100%', width: '100%' }}>
             <Spinner size={SpinnerSize.large} label="Loading..." />
